56cf0b5b60b3d2b3f7877de9908e731f2e39db19
Jeff Dickey <dickeyxxx@gmail.com>
2016-04-23 22:28:19 -0700

    v4.99.0

--- main.go
+++ main.go
@@ -1,151 +1,193 @@
 package main
 
 import (
-	"errors"
+	"crypto/sha256"
+	"encoding/hex"
 	"fmt"
-	"net/http"
-	"net/url"
+	"io"
+	"io/ioutil"
+	"log"
 	"os"
+	"os/exec"
+	"os/user"
+	"path/filepath"
 	"runtime"
-	"runtime/debug"
-	"strings"
+	"syscall"
+	"time"
+
+	"github.com/franela/goreq"
+	"github.com/ulikunitz/xz"
 )
 
-// Version is the version of the v4 cli.
-// This is set by a build flag in the `Rakefile`.
-// If it is set to `dev` it will not autoupdate.
-var Version = "dev"
-
-// GitSHA is the git sha of the build
-// This is set by a build flag in the `Rakefile`.
-var GitSHA = ""
-
-// Channel is the git branch the code was compiled on.
-// This is set by a build flag in the `Rakefile` based on the git branch.
-var Channel = "?"
-
-var cli = &Cli{}
-
-// BuiltinPlugins are the core plugins that will be autoinstalled
-var BuiltinPlugins = []string{
-	"heroku-apps",
-	"heroku-cli-addons",
-	"heroku-fork",
-	"heroku-git",
-	"heroku-local",
-	"heroku-orgs",
-	"heroku-pipelines",
-	"heroku-run",
-	"heroku-spaces",
-	"heroku-status",
+type Manifest struct {
+	ReleasedAt string            `json:"released_at"`
+	Version    string            `json:"version"`
+	Channel    string            `json:"channel"`
+	Builds     map[string]*Build `json:"builds"`
+}
+type Build struct {
+	URL    string `json:"url"`
+	Sha1   string `json:"sha1"`
+	Sha256 string `json:"sha256"`
 }
 
-func init() {
-	cli.Topics = TopicSet{
-		authTopic,
-		commandsTopic,
-		debugTopic,
-		loginTopic,
-		logoutTopic,
-		pluginsTopic,
-		twoFactorTopic,
-		twoFactorTopicAlias,
-		updateTopic,
-		versionTopic,
-		whichTopic,
-		whoamiTopic,
-	}
-	cli.Commands = CommandSet{
-		authLoginCmd,
-		authLogoutCmd,
-		authTokenCmd,
-		commandsListCmd,
-		debugErrlogCmd,
-		loginCmd,
-		logoutCmd,
-		pluginsInstallCmd,
-		pluginsLinkCmd,
-		pluginsListCmd,
-		pluginsUninstallCmd,
-		twoFactorCmd,
-		twoFactorCmdAlias,
-		twoFactorDisableCmd,
-		twoFactorDisableCmdAlias,
-		twoFactorGenerateCmd,
-		twoFactorGenerateCmdAlias,
-		updateCmd,
-		versionCmd,
-		whichCmd,
-		whoamiAuthCmd,
-		whoamiCmd,
-	}
-}
+var DataHome = dataHome()
+var HomeDir = homeDir()
 
 func main() {
-	defer handlePanic()
-	handleSignal(os.Interrupt, func() {
-		if !swallowSigint {
-			showCursor()
-			os.Exit(1)
-		}
-	})
-	runtime.GOMAXPROCS(1) // more procs causes runtime: failed to create new OS thread on Ubuntu
-	ShowDebugInfo()
-	if !(len(os.Args) >= 2 && os.Args[1] == "update") {
-		// skip blocking update if the command is to update
-		// otherwise it will update twice
-		Update(Channel, "block")
-	}
-	SetupNode()
-
-	// try running as a core command
-	cli.Run(os.Args, false)
-
-	// Command wasn't found so load the plugins and try again
-	SetupBuiltinPlugins()
-	TriggerBackgroundUpdate()
-	cli.LoadPlugins(GetPlugins())
-	cli.Run(os.Args, true)
-	Exit(0)
+	loadNewCLI()
+	update()
+	loadNewCLI()
 }
 
-func handlePanic() {
-	if rec := recover(); rec != nil {
-		err, ok := rec.(error)
-		if !ok {
-			err = errors.New(rec.(string))
-		}
-		Errln("ERROR:", err)
-		if Channel == "?" {
-			debug.PrintStack()
+func localAppData() string {
+	return os.Getenv("LOCALAPPDATA")
+}
+
+func homeDir() string {
+	home := os.Getenv("HOME")
+	if home != "" {
+		return home
+	}
+	user, err := user.Current()
+	if err != nil {
+		panic(err)
+	}
+	return user.HomeDir
+}
+
+func dataHome() string {
+	d := os.Getenv("XDG_DATA_HOME")
+	if d == "" {
+		if runtime.GOOS == "windows" && localAppData() != "" {
+			d = localAppData()
 		} else {
-			rollbar(err, "critical")
+			d = filepath.Join(HomeDir, ".local", "share")
 		}
-		TriggerBackgroundUpdate()
-		Exit(1)
 	}
+	return filepath.Join(d, "heroku")
 }
 
-// ShowDebugInfo prints debugging information if HEROKU_DEBUG=1
-func ShowDebugInfo() {
-	if !isDebugging() {
+func loadNewCLI() {
+	bin := filepath.Join(DataHome, "cli", "bin", "heroku")
+	if exists, _ := fileExists(bin); !exists {
 		return
 	}
-	info := []string{version(), binPath}
-	if len(os.Args) > 1 {
-		info = append(info, fmt.Sprintf("cmd: %s", os.Args[1]))
+	if runtime.GOOS == "windows" {
+		cmd := exec.Command(bin, os.Args[1:]...)
+		cmd.Stdin = os.Stdin
+		cmd.Stdout = os.Stdout
+		cmd.Stderr = os.Stderr
+		if err := cmd.Run(); err != nil {
+			os.Exit(getExitCode(err))
+		}
+	} else {
+		if err := syscall.Exec(bin, os.Args[1:], os.Environ()); err != nil {
+			panic(err)
+		}
 	}
-	proxy := getProxy()
-	if proxy != nil {
-		info = append(info, fmt.Sprintf("proxy: %s", proxy))
-	}
-	Debugln(strings.Join(info, " "))
 }
 
-func getProxy() *url.URL {
-	req, err := http.NewRequest("GET", "https://api.heroku.com", nil)
-	WarnIfError(err)
-	proxy, err := http.ProxyFromEnvironment(req)
-	WarnIfError(err)
-	return proxy
+func fileExists(path string) (bool, error) {
+	if _, err := os.Stat(path); err != nil {
+		if os.IsNotExist(err) {
+			return false, nil
+		}
+		return false, err
+	}
+	return true, nil
+}
+
+func getExitCode(err error) int {
+	switch e := err.(type) {
+	case nil:
+		return 0
+	case *exec.ExitError:
+		status, ok := e.Sys().(syscall.WaitStatus)
+		if !ok {
+			panic(err)
+		}
+		return status.ExitStatus()
+	default:
+		panic(err)
+	}
+}
+
+func update() {
+	log.Println("Updating CLI...")
+	manifest := getUpdateManifest("dev")
+	build := manifest.Builds[runtime.GOOS+"-"+runtime.GOARCH]
+	reader, getSha, err := downloadXZ(build.URL)
+	must(err)
+	if getSha() != build.Sha256 {
+		panic(fmt.Errorf("SHA mismatch"))
+	}
+	tmp := tmpDir(DataHome)
+	must(extractTar(reader, tmp))
+	must(os.Rename(filepath.Join(DataHome, "cli"), filepath.Join(tmpDir(DataHome), "heroku")))
+	must(os.Rename(filepath.Join(tmp, "heroku"), filepath.Join(DataHome, "cli")))
+	log.Println("done")
+}
+
+func must(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
+
+func getUpdateManifest(channel string) *Manifest {
+	res, err := goreq.Request{
+		Uri:     "https://cli-assets.heroku.com/" + channel + "/manifest.json",
+		Timeout: 30 * time.Minute,
+	}.Do()
+	must(err)
+	var m Manifest
+	res.Body.FromJsonTo(&m)
+	return &m
+}
+
+func downloadXZ(url string) (io.Reader, func() string, error) {
+	req := goreq.Request{Uri: url, Timeout: 30 * time.Minute}
+	resp, err := req.Do()
+	if err != nil {
+		return nil, nil, err
+	}
+	if err := getHTTPError(resp); err != nil {
+		return nil, nil, err
+	}
+	getSha, reader := computeSha(resp.Body)
+	uncompressed, err := xz.NewReader(reader)
+	return uncompressed, getSha, err
+}
+
+func getHTTPError(resp *goreq.Response) error {
+	if resp.StatusCode < 400 {
+		return nil
+	}
+	var body string
+	body = resp.Header.Get("Content-Type")
+	return fmt.Errorf("%s: %s", resp.Status, body)
+}
+
+func computeSha(reader io.Reader) (func() string, io.Reader) {
+	hasher := sha256.New()
+	tee := io.TeeReader(reader, hasher)
+	getSha := func() string {
+		ioutil.ReadAll(tee)
+		return hex.EncodeToString(hasher.Sum(nil))
+	}
+	return getSha, tee
+}
+
+func tmpDir(base string) string {
+	root := filepath.Join(base, "tmp")
+	err := os.MkdirAll(root, 0755)
+	if err != nil {
+		panic(err)
+	}
+	dir, err := ioutil.TempDir(root, "")
+	if err != nil {
+		panic(err)
+	}
+	return dir
 }
--- vendor/github.com/dickeyxxx/golock/README.md
+++
@@ -1,4 +0,0 @@
-golock
-======
-
-Cross-platform lockfile library for Go.
